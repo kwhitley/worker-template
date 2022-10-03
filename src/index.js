44e69d0d1ff8b82cf2c19cb4ceaca77ccccef68c
Kevin Whitley <krwhitley@gmail.com>
2022-10-03 00:52:55 -0500

    released v0.1.1 - updated template

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { createCors } from 'itty-cors'
 import { Router } from 'itty-router'
 import { error, missing } from 'itty-router-extras'
+import { version } from '../package.json'
 import { routerV1 } from './api/v1'
 
 // export durable object class, per spec
@@ -12,9 +13,10 @@ const { preflight, corsify } = createCors({
 
 const router = Router()
 
-// register v2 API plus all routes
+// register v1 API plus all routes
 router
-  .options('*', preflight)                          // handle CORS OPTIONS requests
+  .options('*', preflight)                              // handle CORS OPTIONS requests
+  .get('/version', () => json({ version }))             // GET API version number
   .all('/v1/*', routerV1.handle)                        // register v1 API
   .all('*', () => missing('Are you sure about that?'))  // 404 for all else
 
